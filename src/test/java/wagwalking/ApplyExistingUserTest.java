6f0cbe45ae66fb864b5023b6426c2b550dda75d1
Vladimir Podoinitsyn <vladimirpodoinitsyn@MacBook-Pro-Vladimir.local>
2020-01-25 19:59:20 -0800

    second commit

--- src/test/java/wagwalking/ApplyExistingUserTest.java
+++ src/test/java/wagwalking/ApplyExistingUserTest.java
@@ -10,16 +10,14 @@ public class ApplyExistingUserTest extends BaseTest {
 
        homePage =  new HomePage(driver);
        applyPage = homePage.clickBookFirstWalkFreeButton();
-
        applyPage.inputEmail("alex@gmail.com");
        applyPage.inputPassword("3151020AA2");
        applyPage.inputFirstName("Vladimir");
        applyPage.inputLastName("Pupkin");
        applyPage.inputPhone("+1322223322");
-
        applyPage.clickNextButton();
 
-//       Thread.sleep(3000); Принудительная задержка в крайнем случае.
+//     Thread.sleep(3000); Принудительная задержка в крайнем случае.
 
        String error = applyPage.getErrorRegisteredEmail();
         Assert.assertEquals("Email address already registered", error);
